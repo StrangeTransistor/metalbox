c6268a38e1e92cc64b629be7ade66d59de547c5e
Strider <strange.transistor@gmail.com>
2016-12-20 00:03:02 +0300

    +Artifact.

---
+++ src/Artifact.js
@@ -0,0 +1,15 @@
+/* @flow */
+
+module.exports
+= function Artifact
+(
+	do_construct /* :DoConstruct */
+)
+	/* :Artifact */
+{
+	var artifact = {}
+
+	artifact.construct = do_construct
+
+	return artifact
+}
