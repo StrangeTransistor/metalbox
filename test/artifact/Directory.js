168d72bdbc990a7800ff65c4d6ba087f37885d84
Strider <strange.transistor@gmail.com>
2016-12-22 22:05:20 +0300

    +test: Directory.

---
+++ test/artifact/Directory.js
@@ -0,0 +1,27 @@
+/* @flow */
+
+var expect = require('chai').expect
+var isDir = require('fs-sync').isDir
+
+var Directory = require('../../src/artifact/Directory')
+
+var tmp_rootpath = require('../_/tmp-rootpath')
+
+describe('Directory', () =>
+{
+	var tmp_root = tmp_rootpath()
+	var tmp_env =
+	{
+		rootpath: tmp_root
+	}
+
+	it('works', () =>
+	{
+		return Directory('test')
+		.construct(tmp_env)
+		.then(() =>
+		{
+			expect(isDir(tmp_root('test'))).true
+		})
+	})
+})
