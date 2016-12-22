4e9d8f1d9e15d51e15d5f4c3e73bfb19292b09ce
Strider <strange.transistor@gmail.com>
2016-12-22 21:42:12 +0300

    +mocha.

---
+++ test/artifact/Artifact.js
@@ -0,0 +1,53 @@
+/* @flow */
+
+var Artifact = require('../../src/artifact/Artifact')
+
+var expect = require('chai').expect
+
+describe('Artifact', () =>
+{
+	it('works', () =>
+	{
+		var side = 0
+
+		var a = Artifact(env =>
+		{
+			side = side + env.inc
+		})
+
+		expect(a).an('object')
+		expect(a).property('construct')
+		expect(a.construct).a('function')
+
+		return a.construct({ inc: 1 })
+		.then(r =>
+		{
+			expect(r).an('undefined')
+			expect(side).eq(1)
+		})
+	})
+
+	it('throws', () =>
+	{
+		var side = 0
+
+		var a = Artifact(env =>
+		{
+			throw new Error
+		})
+
+		expect(a).an('object')
+		expect(a).property('construct')
+		expect(a.construct).a('function')
+
+		return a.construct({ inc: 1 })
+		.then(() =>
+		{
+			expect(0, 'must throw').eq(1)
+		},
+		e =>
+		{
+			expect(e).an('error')
+		})
+	})
+})
