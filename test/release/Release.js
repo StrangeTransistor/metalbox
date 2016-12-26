55cc2f4fc8489f47ac591b3eecdf3fec5c76f8a9
Strider <strange.transistor@gmail.com>
2016-12-27 02:35:37 +0300

    +test: Release.

---
+++ test/release/Release.js
@@ -0,0 +1,47 @@
+/* @flow */
+
+var expect = require('chai').expect
+var isDir  = require('fs-sync').isDir
+var isFile = require('fs-sync').isFile
+var read   = require('fs-sync').read
+
+var File = require('../../src/artifact/File')
+var Directory = require('../../src/artifact/Directory')
+
+var Release = require('../../src/release/Release')
+
+var in_rootpath  = require('../_/in-rootpath')
+var tmp_rootpath = require('../_/tmp-rootpath')
+
+describe('Release', () =>
+{
+	var in_root  = in_rootpath('empty')
+	var tmp_root = tmp_rootpath()
+
+	var tmp_env =
+	{
+		in:  in_root,
+		out: tmp_root
+	}
+
+	it('works', () =>
+	{
+		var r = Release(
+		[
+			Directory('test'),
+			File('test/file', () => 'ABC')
+		])
+
+		return r
+		.construct(tmp_env)
+		.then(() =>
+		{
+			expect(isDir(tmp_root('test'))).true
+			expect(isFile(tmp_root('test/file'))).true
+
+			var content = read(tmp_root('test/file'))
+
+			expect(content).eq('ABC')
+		})
+	})
+})
