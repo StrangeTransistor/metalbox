e6027d79a8af07d20cb757e7c48a74dd801bb328
Strider <strange.transistor@gmail.com>
2016-12-27 02:11:47 +0300

    +test: File.

---
+++ test/artifact/File.js
@@ -0,0 +1,53 @@
+/* @flow */
+
+var expect = require('chai').expect
+
+var isFile = require('fs-sync').isFile
+var read   = require('fs-sync').read
+
+var File = require('../../src/artifact/File')
+
+var tmp_rootpath = require('../_/tmp-rootpath')
+
+describe('File', () =>
+{
+	var tmp_root = tmp_rootpath()
+	var tmp_env =
+	{
+		out: tmp_root
+	}
+
+	it('works', () =>
+	{
+		return File('test', () =>
+		{
+			return 'ABC'
+		})
+		.construct(tmp_env)
+		.then(() =>
+		{
+			expect(isFile(tmp_root('test'))).true
+
+			var content = read(tmp_root('test'))
+
+			expect(content).eq('ABC')
+		})
+	})
+
+	it('works with promise', () =>
+	{
+		return File('test', () =>
+		{
+			return Promise.resolve('DEF')
+		})
+		.construct(tmp_env)
+		.then(() =>
+		{
+			expect(isFile(tmp_root('test'))).true
+
+			var content = read(tmp_root('test'))
+
+			expect(content).eq('DEF')
+		})
+	})
+})
