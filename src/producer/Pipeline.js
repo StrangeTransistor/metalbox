0cf0b8ec28c446e5cd25bdcfcc3dba17fb9ffc76
Strider <strange.transistor@gmail.com>
2017-01-01 22:46:05 +0300

    workon Pipeline.

---
+++ src/producer/Pipeline.js
@@ -0,0 +1,24 @@
+/* @flow */
+
+var slice = [].slice
+
+var reduce = require('bluebird').reduce
+
+module.exports = function Pipeline /* ::<Env, T> */
+(
+	src /* :Producer<Env, T> */
+	/* ::, ...pipes :Array<any> */
+)
+/* :Producer<Env, T> */
+{
+	var pipes = slice.call(arguments, 1)
+
+	return function (env)
+	{
+		return reduce(pipes, (memo, next) =>
+		{
+			return next(memo, env)
+		},
+		src(env))
+	}
+}
