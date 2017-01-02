c0975aea9e121d8732ab5a38e35b1af8638037f4
Strider <strange.transistor@gmail.com>
2017-01-03 02:38:31 +0300

    Pug: use buckets_path.

--- src/producer/Pug.js
+++ src/producer/Pug.js
@@ -3,11 +3,11 @@
 var pug = require('pug').renderFile
 
 module.exports = function Pug ()
-	/* :Producer<EnvRelease, string> */
+	/* :Producer<EnvFrontend, string> */
 {
 	return (env) =>
 	{
-		var filename = env.src('buckets/index/index.pug')
+		var filename = env.src(env.buckets_path, 'index/index.pug')
 
 		var options =
 		{
