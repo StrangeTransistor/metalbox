20d3bf9564d68a9e05be0d573ba1baeb7391d6ec
Strider <strange.transistor@gmail.com>
2017-01-09 01:46:32 +0300

    Artifact: introduce disengage for stop constructing/release resources.

--- src/artifact/Artifact.js
+++ src/artifact/Artifact.js
@@ -1,5 +1,6 @@
 /* @flow */
 
+var resolve  = require('bluebird').resolve
 var resolver = require('bluebird').method
 
 module.exports
@@ -12,6 +13,8 @@ module.exports
 	var artifact = {}
 
 	artifact.construct = resolver(do_construct)
+
+	artifact.disengage = () => resolve()
 
 	return artifact
 }
