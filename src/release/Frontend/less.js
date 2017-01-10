33ef8da5754f63081c286874cc60081206077644
Strider <strange.transistor@gmail.com>
2017-01-10 21:15:36 +0300

    rm debug noise from Frontend/less.

--- src/release/Frontend/less.js
+++ src/release/Frontend/less.js
@@ -3,7 +3,6 @@
 var File  = require('../../artifact/File')
 var Watch = require('../../artifact/Watch')
 var Esc   = require('../../artifact/Esc')
-var Artifact = require('../../artifact/Artifact')
 
 var Pipeline = require('../../producer/Pipeline')
 
@@ -11,7 +10,7 @@ var LessCss = require('../../producer/LessCss')
 var Autoprefixer = require('../../pipe/Autoprefixer')
 var CssNano = require('../../pipe/CssNano')
 
-module.exports.Standard = function ()
+var Standard = module.exports.Standard = function ()
 {
 	return File('index.css', Pipeline(
 		LessCss(),
@@ -21,24 +20,12 @@ module.exports.Standard = function ()
 
 module.exports.Live = function (is_watch /* :any */)
 {
-	var less = module.exports.Standard()
-
-	var art = Artifact(env =>
-	{
-		console.log('artifact start')
-
-		return less
-		.construct(env)
-		.then(() =>
-		{
-			throw new Error('less_error')
-			// console.log('artifact stop')
-		})
-	})
-
-	art.describe = less.describe
-
-	return Esc(is_watch, Watch('.', art))
+	return Esc(
+		is_watch,
+		Watch(
+			'.',
+			Standard()
+		))
 }
 
 module.exports.Min = function ()
