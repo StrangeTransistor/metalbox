ea5addec96590cd3fbbded160e9901fc4b2c2b2e
Strider <strange.transistor@gmail.com>
2017-01-25 20:20:14 +0300

    Babili: fix preset resolution.

--- src/pipe/Babili.js
+++ src/pipe/Babili.js
@@ -7,7 +7,13 @@ module.exports = function Babili ()
 {
 	return (input) /* :string */ =>
 	{
-		var it = babel.transform(input, { presets: [ 'babili' ] })
+		var it = babel.transform(input,
+		{
+			presets:
+			[
+				require.resolve('babel-preset-babili')
+			]
+		})
 
 		return it.code
 	}
