be5c57f8a7ba1ae1833a4775cbb2259ee4ee0dca
Strider <strange.transistor@gmail.com>
2017-02-11 02:36:19 +0300

    runner/run: resolve Artifact.

--- src/runner/run.js
+++ src/runner/run.js
@@ -1,6 +1,16 @@
 /* @flow */
 
-module.exports = (preset_name /* :string */, yargv /* :yargv */) =>
+var findRoot = require('find-root')
+var Rootpath = require('rootpath')
+
+var resolve = require('./_/resolve-shortcut')
+
+module.exports = (release_name /* :string */, yargv /* :yargv */) =>
 {
+	var rootpath = Rootpath(findRoot(process.cwd()))
+
+	var Artifact /* :() => T_Artifact<any> */ = resolve(rootpath, release_name)
+
+	console.log(Artifact)
 	console.log(yargv)
 }
