f779e2091991f8edd0bee2eb26149d494d4225a3
Strider <strange.transistor@gmail.com>
2017-02-14 00:29:47 +0300

    runner: +run (preset without options).

--- src/runner/run.js
+++ src/runner/run.js
@@ -3,14 +3,25 @@
 var findRoot = require('find-root')
 var Rootpath = require('rootpath')
 
+var load = require('fs-sync').readJSON
+
 var resolve = require('./_/resolve-shortcut')
+var run_sealed = require('./_/run-sealed')
 
 module.exports = (release_name /* :string */, yargv /* :yargv */) =>
 {
 	var rootpath = Rootpath(findRoot(process.cwd()))
 
+	var manifest = load(rootpath('package.json'))
+
 	var Artifact /* :() => T_Artifact<any> */ = resolve(rootpath, release_name)
 
-	console.log(Artifact)
-	console.log(yargv)
+	run_sealed(Artifact,
+	{
+		options: {},
+		manifest: manifest,
+		rootpath: rootpath,
+		preset_name: release_name,
+		yargv: yargv
+	})
 }
