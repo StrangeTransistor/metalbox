5e28199714c869a2637179ece7ef2e662c055dcd
Strider <strange.transistor@gmail.com>
2017-02-16 22:17:39 +0300

    Glob/Copy: +test.

--- test/artifact/Glob.js
+++ test/artifact/Glob.js
@@ -3,13 +3,18 @@
 var expect = require('chai').expect
 
 var Glob = require('../../src/artifact/Glob')
+var Copy = require('../../src/artifact/Glob/Copy')
 
 var src_rootpath = require('../_/src-rootpath')
+var dst_rootpath = require('../_/dst-rootpath')
 var tmp_rootpath = require('../_/tmp-rootpath')
+
+var compare = require('../_/compare-release')
 
 describe('Glob', () =>
 {
 	var src_root = src_rootpath('frontend')
+	var dst_root = dst_rootpath('static-only')
 	var tmp_root = tmp_rootpath()
 
 	var tmp_env =
@@ -51,4 +56,15 @@ describe('Glob', () =>
 		expect(r).a('function')
 		expect(r.path).a('string')
 	}
+
+	it('works with fs', () =>
+	{
+		var g = Copy('buckets', '**/*.@(jpg|png|gif)', '')
+
+		return g.construct(tmp_env)
+		.then(() =>
+		{
+			expect(compare(dst_root(), tmp_root())).ok
+		})
+	})
 })
