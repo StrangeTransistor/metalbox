914a502d01a847a6af227cd32834042e1b662ca2
Strider <strange.transistor@gmail.com>
2017-02-17 03:35:57 +0300

    Glob, Glob/Rule: improve tests.

--- test/artifact/Glob.js
+++ test/artifact/Glob.js
@@ -1,9 +1,14 @@
 /* @flow */
 
+var basename = require('path').basename
+
 var expect = require('chai').expect
 
 var Glob = require('../../src/artifact/Glob')
 var Copy = require('../../src/artifact/Glob/Copy')
+var Rule = require('../../src/artifact/Glob/Rule')
+
+var File = require('../../src/artifact/File')
 
 var src_rootpath = require('../_/src-rootpath')
 var dst_rootpath = require('../_/dst-rootpath')
@@ -14,32 +19,31 @@ var compare = require('../_/compare-release')
 describe('Glob', () =>
 {
 	var src_root = src_rootpath('frontend')
-	var dst_root = dst_rootpath('static-only')
-	var tmp_root = tmp_rootpath()
 
-	var tmp_env =
-	{
-		src: src_root,
-		dst: tmp_root
-	}
-
-	var r = []
-
-	var g = Glob('', '**/*.@(jpg|png|gif)', '', (src, path, dst, env) =>
-	{
-		expect_rootpath(src)
-		expect_rootpath(dst)
-
-		expect(env).an('object')
-
-		expect_rootpath(env.src)
-		expect_rootpath(env.dst)
-
-		r.push(path)
-	})
 
 	it('works', () =>
 	{
+		var r = []
+
+		var g = Glob('', '**/*.@(jpg|png|gif)', '', (src, path, dst, env) =>
+		{
+			expect_rootpath(src)
+			expect_rootpath(dst)
+
+			expect(env).an('object')
+
+			expect_rootpath(env.src)
+			expect_rootpath(env.dst)
+
+			r.push(path)
+		})
+
+		var tmp_env =
+		{
+			src: src_root,
+			dst: tmp_rootpath()
+		}
+
 		return g.construct(tmp_env)
 		.then(() =>
 		{
@@ -57,9 +61,48 @@ describe('Glob', () =>
 		expect(r.path).a('string')
 	}
 
+
 	it('works with fs', () =>
 	{
+		var dst_root = dst_rootpath('static-only')
+		var tmp_root = tmp_rootpath()
+
+		var tmp_env =
+		{
+			src: src_root,
+			dst: tmp_root
+		}
+
 		var g = Copy('buckets', '**/*.@(jpg|png|gif)', '')
+
+		return g.construct(tmp_env)
+		.then(() =>
+		{
+			expect(compare(dst_root(), tmp_root())).ok
+		})
+	})
+
+	it('Rule', () =>
+	{
+		var dst_root = dst_rootpath('file-regular-content')
+		var tmp_root = tmp_rootpath()
+
+		var tmp_env =
+		{
+			src: src_root,
+			dst: tmp_root
+		}
+
+		var f = File((env /* :EnvEntry */) =>
+		{
+			return env.entry
+		},
+		(env /* :EnvEntry */) =>
+		{
+			return basename(env.entry, '.js')
+		})
+
+		var g = Rule('buckets', '**/*.js', '', f)
 
 		return g.construct(tmp_env)
 		.then(() =>
