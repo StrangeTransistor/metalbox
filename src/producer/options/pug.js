acc4d55ee02b3f6be6e6d7ad0650033bbc17029d
Tommy Toomean <arttumin@gmail.com>
2017-03-08 23:41:27 +0300

    +refactor Pug func

--- src/producer/options/pug.js
+++ src/producer/options/pug.js
@@ -1,14 +1,11 @@
 /* @flow */
 
-module.exports = function Pug ()
+module.exports = function Pug (env /* :EnvFrontend */)
+	/* :{ pretty: boolean, dev: boolean, basedir: string } */
 {
-	return function (env /* :EnvFrontend */)
-		/* :{ pretty: boolean, dev: boolean, basedir: string } */
-	{
-		return {
-			pretty: false,
-			dev: env.dev || false,
-			basedir: env.src(env.buckets_path)
-		}
+	return {
+		pretty: false,
+		dev: env.dev || false,
+		basedir: env.src(env.buckets_path)
 	}
 }
