59433500193768f109ac313f87643ee1e6afcc5e
Strider <strange.transistor@gmail.com>
2017-03-12 02:06:09 +0300

    +test for Glob(.., Copy()).

--- test/artifact/Glob.js
+++ test/artifact/Glob.js
@@ -5,10 +5,11 @@ var basename = require('path').basename
 var expect = require('chai').expect
 
 var Fn   = require('../../src/artifact/Glob/Fn')
-var Copy = require('../../src/artifact/Glob/Copy')
+var GlobCopy = require('../../src/artifact/Glob/Copy')
 var Glob = require('../../src/artifact/Glob/Glob')
 
 var File = require('../../src/artifact/File')
+var Copy = require('../../src/artifact/Copy')
 
 var src_rootpath = require('../_/src-rootpath')
 var dst_rootpath = require('../_/dst-rootpath')
@@ -72,7 +73,7 @@ describe('Glob (fn)', () =>
 			dst: tmp_root
 		}
 
-		var g = Copy('buckets', '**/*.@(jpg|png|gif)', '')
+		var g = GlobCopy('buckets', '**/*.@(jpg|png|gif)', '')
 
 		return g.construct(tmp_env)
 		.then(() =>
@@ -134,6 +135,26 @@ describe('Glob', () =>
 		})
 
 		var g = Glob('buckets', '**/*.js', 'buckets-transformed', f)
+
+		return g.construct(tmp_env)
+		.then(() =>
+		{
+			expect(compare(dst_root(), tmp_root())).ok
+		})
+	})
+
+	it('works with Copy()', () =>
+	{
+		var dst_root = dst_rootpath('static-only')
+		var tmp_root = tmp_rootpath()
+
+		var tmp_env =
+		{
+			src: src_root,
+			dst: tmp_root
+		}
+
+		var g = Glob('buckets', '**/*.@(jpg|png|gif)', '', Copy())
 
 		return g.construct(tmp_env)
 		.then(() =>
