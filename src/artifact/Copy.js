4b7a01afa2ffcf24fdeda6e863fa6611862ab4c1
Strider <strange.transistor@gmail.com>
2017-03-26 21:29:39 +0300

    Copy: support for watch: unlink.

--- src/artifact/Copy.js
+++ src/artifact/Copy.js
@@ -1,13 +1,43 @@
 /* @flow */
+/* ::
+
+type EnvCopy = EnvEntryEvent | EnvEntry;
+
+export type Options =
+{
+	unlink?: boolean
+};
+
+*/
+
+var assign = Object.assign
 
 var cp = require('fs-sync').copy
+var rm = require('fs-sync').remove
 
 var Artifact = require('./Artifact')
 
-module.exports = function Copy /* ::<Env: EnvInOut & EnvEntry> */ ()
+module.exports = function Copy /* ::<Env: EnvInOut & EnvCopy> */
+(options /* ::?:Options */)
 {
+	options = assign({ unlink: true }, options)
+
 	return Artifact((env /* :Env */) =>
 	{
-		return cp(env.src(env.entry), env.dst(env.entry), { force: true })
+		/* @flow-off */
+		if (env.event && (env.event === 'unlink'))
+		{
+			/* @flow-off */
+			if (options.unlink)
+			{
+				console.log('RM')
+				rm(env.dst(env.entry))
+			}
+		}
+		else
+		{
+			console.log('+')
+			cp(env.src(env.entry), env.dst(env.entry), { force: true })
+		}
 	})
 }
