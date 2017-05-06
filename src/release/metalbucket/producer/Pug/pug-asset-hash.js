3b4b36347675c9f11afbc77794ec8ee98b46f197
Strider <strange.transistor@gmail.com>
2017-05-07 00:45:15 +0300

    Pug: asset.url.

--- src/release/metalbucket/producer/Pug/pug-asset-hash.js
+++ src/release/metalbucket/producer/Pug/pug-asset-hash.js
@@ -1,24 +1,38 @@
 /* @flow */
 
+var Asset_url = require('../../asset-url')
+
 module.exports = (env /* :EnvFrontend */) =>
 {
 	/* @flow-off */
 	var hash = env.hash
 
+	var asset
+
 	if (hash)
 	{
-		return (url /* :string */) =>
+		asset = (url /* :string */) =>
 		{
 			return url.replace(re_asset(), '$1' + '.' + hash + '.' + '$2')
 		}
 	}
 	else
 	{
-		return (url /* :string */) =>
+		asset = (url /* :string */) =>
 		{
 			return url
 		}
 	}
+
+
+	var asset_url = Asset_url(hash)
+
+	asset.url = (url /* :string */) =>
+	{
+		return `url(${ asset_url(url) })`
+	}
+
+	return asset
 }
 
 var re_asset = () => /^(.+)\.([^.]+)$/
