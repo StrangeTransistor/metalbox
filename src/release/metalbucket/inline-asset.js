bf13dfc27b379997df33339fa9b4e53dcd13955a
Strider <strange.transistor@gmail.com>
2017-05-22 02:23:14 +0300

    assets: inline resources.

--- src/release/metalbucket/inline-asset.js
+++ src/release/metalbucket/inline-asset.js
@@ -2,8 +2,10 @@
 
 var threshold = 14 * 1024
 
+var read = require('fs').readFileSync
+
 var isFile = require('fs-sync').isFile
-var read = require('fs-sync').read
+var mime = require('mime')
 
 module.exports = function inline (filename /* :string */)
 {
@@ -12,14 +14,14 @@ module.exports = function inline (filename /* :string */)
 		return null
 	}
 
-	var content = new Buffer(read(filename))
+	var content = new Buffer.from(read(filename))
 
 	if (content.length > threshold)
 	{
 		return null
 	}
 
-	var type = 'image/png'
+	var type = mime.lookup(filename)
 
 	return `data:${ type };base64,${ content.toString('base64') }`
 }
