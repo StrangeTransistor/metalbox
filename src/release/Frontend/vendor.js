296b427e981fae9226c32298c592b75dafcf7f67
Strider <strange.transistor@gmail.com>
2017-06-12 02:09:29 +0300

    Vendor: support assets dir.

--- src/release/Frontend/vendor.js
+++ src/release/Frontend/vendor.js
@@ -1,12 +1,32 @@
 /* @flow */
 
+var Composite = require('../../artifact/Composite')
+
 // var Watch = require('../../artifact/Watch')
 var Glob = require('../../artifact/Glob')
 var Copy = require('../../artifact/Copy')
+
+var asset_dir = require('../metalbucket/asset-dir')
+
 
 var glob = [ '*', '!~assets' ]
 
 module.exports.Standard = function ()
 {
+	return Composite([ Flat(), Assets() ])
+}
+
+function Flat ()
+{
 	return Glob(env => env.buckets('vendor'), glob, '', Copy())
+}
+
+function Assets ()
+{
+	return Glob(
+		env => env.buckets('vendor/~assets'),
+		'*',
+		env => asset_dir(env.hash),
+		Copy()
+	)
 }
