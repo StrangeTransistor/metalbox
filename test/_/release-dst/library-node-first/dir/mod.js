fdcb2009b9222068486d6078ab9858b365382b6f
Strider <strange.transistor@gmail.com>
2017-07-11 02:26:31 +0300

    Library: +tests for targets cases.

---
+++ test/_/release-dst/library-node-first/dir/mod.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/*  */
+
+function sqr (v)
+{
+	return v * v
+}
+
+module.exports = sqr;
--- test/release/library/Library.js
+++ test/release/library/Library.js
@@ -16,15 +16,39 @@ var expect_release = require('../../_/expect-release')
 describe('Library', () =>
 {
 	var src_root = src_rootpath('library')
-	var dst_root = dst_rootpath('library')
-	var tmp_root = tmp_rootpath()
 
-	var env = tmp_env(src_root, tmp_root)
-
-	var rl = Library()
-
-	it('works', () =>
+	it('works with jsnext-first', () =>
 	{
+		return test_library('jsnext-first', 'library')
+	})
+
+	it('works with node-first', () =>
+	{
+		return test_library('node-first', 'library-node-first')
+	})
+
+	it('works with node-only', () =>
+	{
+		return test_library('node-only', 'library-node-only')
+	})
+
+	it('works with jsnext-only', () =>
+	{
+		return test_library('jsnext-only', 'library-jsnext-only')
+	})
+
+	function test_library (mode, dst)
+	{
+		var dst_root = dst_rootpath(dst)
+		var tmp_root = tmp_rootpath()
+
+		var env = tmp_env(src_root, tmp_root)
+
+		/* @flow-off */
+		env.options = { targets: mode }
+
+		var rl = Library()
+
 		return rl.construct(env)
 		.then(() =>
 		{
@@ -34,5 +58,5 @@ describe('Library', () =>
 		{
 			expect_release(tmp_root('release.json'))
 		})
-	})
+	}
 })
