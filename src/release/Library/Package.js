4065bd27d044619fd7bb2f31eb15dded205e57d3
Strider <strange.transistor@gmail.com>
2017-07-12 02:26:48 +0300

    Library/Package: put proper entry points into package.json.

--- src/release/Library/Package.js
+++ src/release/Library/Package.js
@@ -2,27 +2,47 @@
 
 var join = require('path').join
 
+var find = require('lodash/find')
+
+var Artifact = require('../../artifact/Artifact')
 var File = require('../../artifact/File')
+
 var WithPackage = require('../../producer/WithPackage')
+
+var from_targets = require('./rollup-targets')
 
 module.exports = function Package /* ::<Env: EnvRelease>*/ ()
 	/* :T_Artifact<Env> */
 {
-	return File('package.json', WithPackage(manifest =>
+	return Artifact(env =>
 	{
-		delete manifest.metalbox
+		var targets = from_targets(env)
 
-		var main = manifest.main || ''
+		var node   = find(targets, such => such[0] === 'node')
+		var jsnext = find(targets, such => such[0] === 'jsnext')
 
-		// TODO library proper dist, main
-		manifest.main = join('dist', main)
-
-		if (main)
+		return File('package.json', WithPackage(manifest =>
 		{
-			manifest['jsnext:main'] = main
-			manifest.module = main
-		}
+			delete manifest.metalbox
 
-		return manifest
-	}))
+			var base = manifest.main || ''
+			var main_jsnext
+
+			if (node)
+			{
+				manifest.main = join(node[1], base)
+			}
+
+			if (jsnext)
+			{
+				var main_jsnext = join(jsnext[1], base)
+
+				manifest['jsnext:main'] = main_jsnext
+				manifest.module = main_jsnext
+			}
+
+			return manifest
+		}))
+		.construct(env)
+	})
 }
