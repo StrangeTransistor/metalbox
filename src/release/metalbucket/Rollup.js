9f5bb1760acbba3feefdaac0ba60ee4615a6c6a3
Strider <strange.transistor@gmail.com>
2017-09-04 22:04:32 +0300

    prepare File-Rollup to eat .ts extension.

--- src/release/metalbucket/Rollup.js
+++ src/release/metalbucket/Rollup.js
@@ -1,5 +1,8 @@
 /* @flow */
 
+var parse  = require('path').parse
+var format = require('path').format
+
 var File = require('../../artifact/File')
 var Rollup = require('../../producer/rollup/Backend')
 
@@ -7,5 +10,25 @@ var label = require('../../label')
 
 module.exports = () =>
 {
-	return label('Rollup', File(env => env.entry, Rollup()))
+	return label('Rollup', File(entry, Rollup()))
+}
+
+var entry = (env) => to_js(env.entry)
+
+function to_js (filename /* :string */) /* :string */
+{
+	var parsed = parse(filename)
+
+	if (parsed.ext === '.js')
+	{
+		return filename
+	}
+	else
+	{
+		parsed.ext = '.js'
+		parsed.base = ''
+
+		/* @flow-off */
+		return format(parsed)
+	}
 }
