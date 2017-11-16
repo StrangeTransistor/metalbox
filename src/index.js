4a11fea7d38f0361669d97758b1f016785bd4171
Strider <strange.transistor@gmail.com>
2017-11-17 02:17:55 +0300

    use async/await and node >= 7.6.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 /* @flow */
 
-export default function runner (...args /*: string[] */)
+export default async function runner (...args /*: string[] */)
 {
 	console.log(args)
+	console.log(await product())
+}
+
+async function product ()
+{
+	return 'product'
 }
