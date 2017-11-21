cc39c0e90fac43e10e4bf697d1f935e03be51d47
Strider <strange.transistor@gmail.com>
2017-11-22 00:22:39 +0300

    mkdir.

--- src/tmp.js
+++ src/tmp.js
@@ -3,10 +3,15 @@
 import { tmpdir } from 'os'
 import { generate as random } from 'randomstring'
 import rootpath from '@streetstrider/rootpath'
+import { sync as mkdir } from 'mkdirp'
 
 export default function tmp ()
 {
 	var hash = random({ length: 5, capitalization: 'uppercase' })
 
-	return rootpath(tmpdir(), 'metalbox', hash)
+	var fromroot = rootpath(tmpdir(), 'metalbox', hash)
+
+	mkdir(fromroot())
+
+	return fromroot
 }
