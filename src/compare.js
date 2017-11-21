2c08a2fb5a323cc987aef6eb2c1cb1538637e87b
Strider <strange.transistor@gmail.com>
2017-11-22 00:35:30 +0300

    dir-compare.

---
+++ src/compare.js
@@ -0,0 +1,32 @@
+/* @flow */
+
+import clc from 'cli-color'
+import { compareSync as compare } from 'dir-compare'
+
+var bold = clc.bold
+
+export default function (dst, tmp)
+{
+	var r = compare(dst, tmp,
+	{
+		compareSize: true,
+		compareContent: true,
+		excludeFilter: 'release.json'
+	})
+
+	if (r.same)
+	{
+		console.log(` ${bold('tmp:')} ${tmp}`)
+	}
+	else
+	{
+		var differences = r.differences
+		var diff = r.diffSet.filter(it => it.state !== 'equal')
+
+		console.warn('release differs: %s files differences', differences)
+		console.warn(diff)
+		console.log(bold(` meld ${tmp} ${dst}`))
+
+		throw new Error('release differs')
+	}
+}
