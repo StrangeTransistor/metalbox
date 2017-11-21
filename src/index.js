c4b64c0cb8c3d972593197107363e9cc3c5dd26d
Strider <strange.transistor@gmail.com>
2017-11-22 00:36:58 +0300

    test with input dirs.

--- src/index.js
+++ src/index.js
@@ -1,32 +1,8 @@
 /* @flow */
 
 // import Unit from './Unit'
-import File from './Unit/File'
-
-import tmp from './tmp'
 
 export default async function runner (...args /*: string[] */)
 {
 	console.log(args)
-
-	var unit = File('/tmp/abc', 'ABC')
-
-	var context =
-	{
-		input: null,
-
-		first: true,
-		once: false,
-		live: false,
-
-		engine: {},
-		storage: {},
-	}
-
-	console.log(await unit(context))
-
-	var t = tmp()
-
-	console.log(t())
-	console.log(t('abc'))
 }
--- test/case/Unit/File.test.js
+++ test/case/Unit/File.test.js
@@ -1,8 +1,35 @@
 /* @flow */
 
+import rootpath from '@streetstrider/rootpath'
+
+import tmp from '../../../src/tmp'
 import File from '../../../src/Unit/File'
+
+import compare from '../../../src/compare'
 
 describe('File', () =>
 {
-	console.log('!', File)
+	it('File(str, str)', async () =>
+	{
+		var cl = rootpath(__dirname, '../../collate/file/1')
+		var tm = tmp()
+
+		var unit = File(tm('abc'), 'content\n')
+
+		var context =
+		{
+			input: null,
+
+			first: true,
+			once: false,
+			live: false,
+
+			engine:  {},
+			storage: {},
+		}
+
+		await unit(context)
+
+		compare(cl(), tm())
+	})
 })
