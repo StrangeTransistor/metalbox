c1c9026325555e7330e9bd35493580238f06d9ab
Strider <strange.transistor@gmail.com>
2017-11-25 23:34:01 +0300

    fix collate name.

--- src/collate.js
+++ src/collate.js
@@ -2,7 +2,7 @@
 
 import rootpath from '@streetstrider/rootpath'
 
-export default function tmp (name /* :string */)
+export default function collate (name /* :string */)
 {
 	return rootpath(__dirname, '..', 'test/collate', name)
 }
