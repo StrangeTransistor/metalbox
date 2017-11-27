f538b9bd67df1d6d0157ef63d824eb53ef8425d7
Strider <strange.transistor@gmail.com>
2017-11-28 01:59:08 +0300

    test/Rollup: bare test with expect value.

--- test/case/Unit/Rollup.test.js
+++ test/case/Unit/Rollup.test.js
@@ -1,15 +1,16 @@
 /* @flow */
 
-// import bluebird from 'bluebird'
-// var delay = bluebird.delay
+import { expect } from 'chai'
 
 // import tmp from 'src/tmp'
 // import collate from 'src/collate'
-// import compare from 'src/compare'
 import origin from 'src/origin'
+
+// import compare from 'src/compare'
 
 import Context from 'src/Context'
 import Rollup from 'src/Unit/Rollup/Rollup'
+// import File from 'src/Unit/File'
 
 describe('Rollup', () =>
 {
@@ -22,6 +23,11 @@ describe('Rollup', () =>
 
 		var outcome = await unit(context)
 
-		console.log(outcome)
+		expect(outcome).an('object')
+		expect(outcome.output).an('object')
+
+		// eslint-disable-next-line max-len
+		expect(outcome.output.content).eq("'use strict';\n\nfunction main ()\n{\n\tconsole.log('main');\n}\n\nmodule.exports = main;\n")
+		expect(outcome.output.sourcemap).null
 	})
 })
