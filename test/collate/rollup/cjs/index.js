d02b274f8a4235953a8b364d8baae59ee8a300fb
Strider <strange.transistor@gmail.com>
2017-12-04 00:08:17 +0300

    Rollup: check dependencies.

--- test/collate/rollup/cjs/index.js
+++ test/collate/rollup/cjs/index.js
@@ -1,8 +1,13 @@
 'use strict';
 
+function mod ()
+{
+	return 'mod'
+}
+
 function main ()
 {
-	console.log('main');
+	console.log('main', mod());
 }
 
 module.exports = main;
--- test/origin/rollup/index.js
+++ test/origin/rollup/index.js
@@ -1,5 +1,7 @@
 
+import mod from './mod'
+
 export default function main ()
 {
-	console.log('main')
+	console.log('main', mod())
 }
