283d1b49d1a8e5e35d3bfc4567d859b85dd61300
Strider <strange.transistor@gmail.com>
2017-12-06 01:12:25 +0300

    Pipe: test.

--- test/case/Unit/Pipe.test.js
+++ test/case/Unit/Pipe.test.js
@@ -3,10 +3,36 @@
 import { expect } from 'chai'
 
 import Unit from 'src/Unit'
+import Pipe from 'src/Unit/compose/Pipe'
+
 import Context from 'src/Context'
 
-describe('pipe / before', () =>
+describe('Pipe / Unit.pipe / Unit.before', () =>
 {
+	it('Pipe(u1, u2)', async () =>
+	{
+		var u1 = Unit(context =>
+		{
+			var x /* :number */ = context.input.x
+			return { x }
+		})
+
+		var u2 = Unit(context =>
+		{
+			var x /* :number */ = context.input.x
+
+			return { y: x + 2 }
+		})
+
+		var u = Pipe(u1, u2)
+
+		var context = Context({ x: 5 })
+
+		var outcome = await u(context)
+
+		expect(outcome.output).deep.eq({ y: 7 })
+	})
+
 	it('u1.pipe(u2)', async () =>
 	{
 		var u1 = Unit(context =>
