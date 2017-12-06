04fa0042fe832acbc2cd607186d105d5a0061744
Strider <strange.transistor@gmail.com>
2017-12-06 18:44:52 +0300

    File: support pre mkdirp.

--- src/Unit/File.js
+++ src/Unit/File.js
@@ -1,25 +1,43 @@
 /* @flow */
 
+var assign = Object.assign
+
 import { isAbsolute as is_abs } from 'path'
+import { dirname } from 'path'
+
 import { writeFile  as write }  from 'mz/fs'
 
 import bluebird from 'bluebird'
 var join = bluebird.join
 
+import mkdirp from 'mkdirp-promise'
+
 import Unit from './Unit'
 
 import unroll from '../unroll'
 
+/* ::
+
+type $File$Options =
+{
+	mkdirp: boolean,
+}
+
+*/
+
 export default function File /* ::<$in> */
 (
 	filename /* :$Computable<$in, string> */,
-	content  /* :$Computable<$in, string> */
+	content  /* :$Computable<$in, string> */,
+	options  /* :$Shape<$File$Options> */
 )
 	/* :$Unit<$in, void> */
 {
+	var Σoptions = assign({ mkdirp: true }, options)
+
 	return Unit(async (context) =>
 	{
-		var filename_u = prep_path(context, filename)
+		var filename_u = prep_path(context, filename, Σoptions)
 		var content_u  = unroll(context, content)
 
 		; [ filename_u, content_u ] = await join(filename_u, content_u)
@@ -37,10 +55,18 @@ export default function File /* ::<$in> */
 async function prep_path /* ::<$in> */
 (
 	context  /* :$Context<$in> */,
-	filename /* :$Computable<$in, string> */
+	filename /* :$Computable<$in, string> */,
+	options  /* :$File$Options */
 )
 {
-	return unroll(context, filename)
+	var Σfilename = await unroll(context, filename)
+
+	if (options.mkdirp)
+	{
+		await mkdirp(dirname(Σfilename))
+	}
+
+	return Σfilename
 }
 
 
