07433dfe6cc34bf94eef0f6d0f8e7100f7045276
Strider <strange.transistor@gmail.com>
2017-12-18 22:48:13 +0300

    ridof distinct packages mkdirp, mkdirp-promise.

--- flow-typed/npm/mkdirp-promise_vx.x.x.js
+++
@@ -1,8 +0,0 @@
-// flow-typed signature: c04c3a4262791510d3d72c3eaff80d89
-// flow-typed version: <<STUB>>/mkdirp-promise_v5/flow_v0.59.0
-
-declare module 'mkdirp-promise' {
-  declare type Options = number | { mode?: number; fs?: mixed };
-
-  declare module.exports: (path: string, options?: Options) => Promise<void>;
-}
