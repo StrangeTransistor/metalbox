db18ed50057468005e733ae01781badf96ff2b41
Strider <strange.transistor@gmail.com>
2018-01-11 17:29:16 +0300

    drain: either and finalize.

--- src/drain.js
+++ src/drain.js
@@ -2,7 +2,8 @@
 
 import Promise from 'bluebird'
 
-import { on } from 'flyd'
+import { on }  from 'flyd'
+import { combine } from 'flyd'
 
 export default function drain /* ::<$value> */
 (
@@ -14,4 +15,52 @@ export default function drain /* ::<$value> */
 	{
 		on(() => rs(stream()), stream.end)
 	})
+}
+
+export function either /* ::<$value, $error: Error> */
+(
+	stream /* :flyd$Stream<$value | $error> */
+)
+	/* :Promise<$value> */
+{
+	return new Promise((rs, rj) =>
+	{
+		on(decide, stream.end)
+
+		function decide ()
+		{
+			var value = stream()
+
+			if (value instanceof Error)
+			{
+				rj(value)
+			}
+			else
+			{
+				rs(value)
+			}
+		}
+	})
+}
+
+
+export function finalize /* ::<$value> */
+(
+	stream /* :flyd$Stream<$value> */
+)
+	/* :flyd$Stream<$value> */
+{
+	return combine(handle, [ stream ])
+
+	function handle (stream, self)
+	{
+		var value = stream()
+
+		self(value)
+
+		if (value instanceof Error)
+		{
+			self.end(true)
+		}
+	}
 }
--- test/case/drain.test.js
+++ test/case/drain.test.js
@@ -5,6 +5,8 @@ import { stream } from 'flyd'
 import { expect } from 'chai'
 
 import drain from 'src/drain'
+import { either } from 'src/drain'
+import { finalize } from 'src/drain'
 
 describe('drain', () =>
 {
@@ -42,5 +44,174 @@ describe('drain', () =>
 		var r = await drain(s)
 
 		expect(r).eq(17)
+	})
+})
+
+
+describe('drain.either', () =>
+{
+	it('single value', async () =>
+	{
+		var s = stream()
+
+		s(17)
+		s.end(true)
+
+		var r = await either(s)
+
+		expect(r).eq(17)
+	})
+
+	it('single value', async () =>
+	{
+		var s = stream(17)
+		s.end(true)
+
+		var r = await either(s)
+
+		expect(r).eq(17)
+	})
+
+	it('last value', async () =>
+	{
+		var s = stream()
+
+		s(1)
+		s(11)
+		s(17)
+		s.end(true)
+
+		var r = await either(s)
+
+		expect(r).eq(17)
+	})
+
+	it('single error', async () =>
+	{
+		var s = stream()
+		var error = new Error('e')
+
+		s(error)
+		s.end(true)
+
+		var r = await either(s).then(
+		()  => expect(false).true,
+		(e) => e)
+
+		expect(r).eq(error)
+	})
+
+	it('single error', async () =>
+	{
+		var error = new Error('e')
+		var s = stream(error)
+		s.end(true)
+
+		var r = await either(s).then(
+		()  => expect(false).true,
+		(e) => e)
+
+		expect(r).eq(error)
+	})
+
+	it('values then error', async () =>
+	{
+		var s = stream()
+		var error = new Error('e')
+
+		s(1)
+		s(11)
+		s(17)
+		s(error)
+		s.end(true)
+
+		var r = await either(s).then(
+		()  => expect(false).true,
+		(e) => e)
+
+		expect(r).eq(error)
+	})
+
+	it('multiple errors (without finalization)', async () =>
+	{
+		var s = stream()
+		var error = new Error('e')
+
+		s(new Error('not'))
+		s(new Error('that'))
+		s(error)
+		s.end(true)
+
+		var r = await either(s).then(
+		()  => expect(false).true,
+		(e) => e)
+
+		expect(r).eq(error)
+	})
+})
+
+
+describe('finalize drain.either', () =>
+{
+	it('single error', async () =>
+	{
+		var s = stream()
+		var error = new Error('e')
+
+		s(error)
+
+		var r = await either(finalize(s)).then(
+		()  => expect(false).true,
+		(e) => e)
+
+		expect(r).eq(error)
+	})
+
+	it('single error', async () =>
+	{
+		var error = new Error('e')
+		var s = stream(error)
+
+		var r = await either(finalize(s)).then(
+		()  => expect(false).true,
+		(e) => e)
+
+		expect(r).eq(error)
+	})
+
+	it('values then error', async () =>
+	{
+		var s = stream()
+		var error = new Error('e')
+
+		s(1)
+		s(11)
+		s(17)
+		s(error)
+
+		var r = await either(finalize(s)).then(
+		()  => expect(false).true,
+		(e) => e)
+
+		expect(r).eq(error)
+	})
+
+	it('multiple errors', async () =>
+	{
+		var s = stream()
+		var error = new Error('e')
+
+		setTimeout(() =>
+		{
+			s(error)
+			s(new Error('not'))
+			s(new Error('that'))
+		})
+
+		var r = await either(finalize(s)).then(
+		()  => expect(false).true,
+		(e) => e)
+
+		expect(r).eq(error)
 	})
 })
