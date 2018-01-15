81d482594aff56e3365e614b81b3743425c73d51
Strider <strange.transistor@gmail.com>
2018-01-15 23:39:05 +0300

    workon Pipe streaming.

--- test/case/Unit/Pipe.test.js
+++ test/case/Unit/Pipe.test.js
@@ -2,6 +2,8 @@
 
 import { expect } from 'chai'
 
+import { stream } from 'flyd'
+
 import Unit from 'src/Unit'
 import Pipe from 'src/Unit/compose/Pipe'
 
@@ -55,5 +57,51 @@ describe('Pipe / Unit.pipe', () =>
 		var output = await u(context).output
 
 		expect(output).deep.eq({ y: 7 })
+	})
+
+	it.only('(stream u1).pipe(u2)', async () =>
+	{
+		var u1 = Unit(() =>
+		{
+			var s = stream({ x: 1 })
+
+			var s = stream()
+
+			// eslint-disable-next-line max-nested-callbacks
+			setTimeout(() =>
+			{
+				s({ x: 1 })
+				s({ x: 2 })
+				s({ x: 3 })
+				s.end(true)
+			}
+			, 100)
+
+			return s
+		})
+
+		var u2 = Unit((input) =>
+		{
+			var x /* :number */ = input.x
+			// console.log('!', x)
+
+			return x + 2
+		})
+
+		var u = u1.pipe(u2)
+
+		var context = Context(null)
+
+		var buffer = []
+
+		var outcome = u(context)
+
+		/* @flow-off */
+		outcome.stream.map(x => buffer.push(x))
+
+		var output = await outcome.output
+
+		expect(buffer).deep.eq([ 3, 4, 5 ])
+		expect(output).eq(5)
 	})
 })
