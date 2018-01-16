c96dbf8a130aa4c537ea788756c80762b041d7fa
Strider <strange.transistor@gmail.com>
2018-01-17 02:32:10 +0300

    Precursor: streaming tests and copy-paste implementation (from Pipe).

--- src/Unit/compose/Precursor.js
+++ src/Unit/compose/Precursor.js
@@ -1,8 +1,14 @@
 /* @flow */
 
+var noop = () => {}
+
+import { on } from 'flyd'
+import { combine } from 'flyd'
+
 import Unit from '../Unit'
 
-export default function Precursor /* ::<$in, $prov: $Providers$Base, $out> */
+export default function Precursor
+	/* ::<$in, $prov: $Providers$Base, $out> */
 (
 	u1 /* :$Unit<$in, $prov, any>  */,
 	u2 /* :$Unit<$in, $prov, $out> */
@@ -11,10 +17,53 @@ export default function Precursor /* ::<$in, $prov: $Providers$Base, $out> */
 {
 	return Unit((_, context) =>
 	{
-		var u = u1.pipe(Unit(() => context.input)).pipe(u2)
+		var out1 = u1(context)
 
-		var outcome = u(context)
+		if (out1.stream)
+		{
+			var context_live  = context.derive(context.input)
+			context_live.live = true
 
-		return outcome.stream || outcome.output
+			/* @flow-off */
+			var stream = combine(handle, [ out1.stream ])
+
+			function handle (prev, self)
+			{
+				var value = prev()
+
+				if (value instanceof Error)
+				{
+					self(value)
+				}
+				else
+				{
+					/* TODO: stream in stream */
+					u2(context_live).output
+					.catch((error) =>
+					{
+						if (! (error instanceof Error)) error = Error(error)
+
+						return error
+					})
+					.then(self)
+					// out2.stream +
+				}
+			}
+
+			/* @flow-off */
+			on(out1.stream.end, stream.end)
+			// out2.stream +
+
+			out1.output.catch(noop)
+
+			return stream
+		}
+		else
+		{
+			return out1.output.then(() =>
+			{
+				return u2(context).output
+			})
+		}
 	})
 }
--- test/case/Unit/Precursor.test.js
+++ test/case/Unit/Precursor.test.js
@@ -2,6 +2,13 @@
 
 import { expect } from 'chai'
 
+import Promise from 'bluebird'
+var delay = Promise.delay
+
+import { stream } from 'flyd'
+
+import { concat } from 'src/drain'
+
 import Unit from 'src/Unit'
 import Precursor from 'src/Unit/compose/Precursor'
 
@@ -78,5 +85,96 @@ describe('Precursor', () =>
 		var output = await u(context).output
 
 		expect(output).deep.eq({ y: '5a' })
+	})
+
+	it('(stream u1).pre(u2)', async () =>
+	{
+		var u1 = Unit(() =>
+		{
+			var s = stream({ x: 1 })
+
+			// eslint-disable-next-line max-nested-callbacks
+			delay(50).then(() =>
+			{
+				s({ x: 2 })
+				s({ x: 3 })
+			})
+			.delay(50)
+			// eslint-disable-next-line max-nested-callbacks
+			.then(() =>
+			{
+				s.end(true)
+			})
+
+			return s
+		})
+
+		var u2 = Unit(input =>
+		{
+			var x /* :string */ = input.x
+
+			return x + 'a'
+		})
+
+		var u = u1.pre(u2)
+		var context = Context({ x: '5' })
+		var outcome = u(context)
+
+		/* @flow-off */
+		var buffer = concat(outcome.stream)
+		var output = outcome.output
+
+		expect(await output).eq('5a')
+		expect(await buffer).deep.eq([ '5a', '5a', '5a' ])
+	})
+
+	it('(stream u1 Error).pre(u2)', async () =>
+	{
+		var error = new Error('e')
+
+		/* eslint-disable max-nested-callbacks */
+		var u1 = Unit(() =>
+		{
+			var s = stream({ x: 1 })
+
+			delay(50).then(() =>
+			{
+				s({ x: 2 })
+			})
+			.delay(50)
+			.then(() =>
+			{
+				s(error)
+			})
+			.delay(50)
+			.then(() =>
+			{
+				/* check for dup */
+				s(error)
+			})
+
+			return s
+		})
+		/* eslint-enable max-nested-callbacks */
+
+		var u2 = Unit(input =>
+		{
+			var x /* :string */ = input.x
+
+			return x + 'a'
+		})
+
+		var u = u1.pre(u2)
+		var context = Context({ x: '5' })
+		var outcome = u(context)
+
+		/* @flow-off */
+		var buffer = concat(outcome.stream)
+		var r = outcome.output.then(
+		()  => expect(false).true,
+		(e) => e)
+
+		expect(await r).eq(error)
+		expect(await buffer).deep.eq([ '5a', '5a', error ])
 	})
 })
