8e7e7332ff40635bf222f64b23e52e5123f41dc9
Strider <strange.transistor@gmail.com>
2018-02-12 03:05:42 +0300

    backpressure/test: improvements.

--- test/case/backpressure.test.js
+++ test/case/backpressure.test.js
@@ -1,4 +1,5 @@
 /* @flow */
+/* eslint-disable max-statements */
 
 import { expect } from 'chai'
 
@@ -16,8 +17,17 @@ describe('backpressure', () =>
 		var s = stream()
 		var b = backpressure(s)
 
-		var b1 = []
-		var b2 = []
+		var b_s1 = []
+		var b_b1 = []
+
+		var b_s2 = []
+		var b_b2 = []
+
+		s.map(v => b_s1.push(v))
+		b.map(v => b_b1.push(v))
+
+		s.end.map(() => b_s1.push('end'))
+		b.end.map(() => b_b1.push('end'))
 
 		await delay(25)
 		.then(() => s(1))
@@ -28,19 +38,67 @@ describe('backpressure', () =>
 		.delay(25)
 		.then(() => s.end(true))
 
-		s.map(v => b1.push(v))
-		b.map(v => b2.push(v))
+		s.map(v => b_s2.push(v))
+		b.map(v => b_b2.push(v))
 
-		s.end.map(() => b1.push('end'))
-		b.end.map(() => b2.push('end'))
+		s.end.map(() => b_s2.push('end'))
+		b.end.map(() => b_b2.push('end'))
 
 		b.continue()
 		b.continue()
 		b.continue()
 		b.continue()
 
-		expect(b1).deep.eq([ 3, 'end' ])
-		expect(b2).deep.eq([ 1, 2, 3, 'end' ])
+		expect(b_s1).deep.eq([ 1, 2, 3, 'end' ])
+		expect(b_b1).deep.eq([ 1, 2, 3, 'end' ])
+
+		expect(b_s2).deep.eq([ 3, 'end' ])
+		expect(b_b2).deep.eq([ 1, 2, 3, 'end' ])
+	})
+
+	it('stores data (init value)', async () =>
+	{
+		var s = stream(0)
+		var b = backpressure(s)
+
+		var b_s1 = []
+		var b_b1 = []
+
+		var b_s2 = []
+		var b_b2 = []
+
+		s.map(v => b_s1.push(v))
+		b.map(v => b_b1.push(v))
+
+		s.end.map(() => b_s1.push('end'))
+		b.end.map(() => b_b1.push('end'))
+
+		await delay(25)
+		.then(() => s(1))
+		.delay(25)
+		.then(() => s(2))
+		.delay(25)
+		.then(() => s(3))
+		.delay(25)
+		.then(() => s.end(true))
+
+		s.map(v => b_s2.push(v))
+		b.map(v => b_b2.push(v))
+
+		s.end.map(() => b_s2.push('end'))
+		b.end.map(() => b_b2.push('end'))
+
+		b.continue()
+		b.continue()
+		b.continue()
+		b.continue()
+		b.continue()
+
+		expect(b_s1).deep.eq([ 0, 1, 2, 3, 'end' ])
+		expect(b_b1).deep.eq([ 0, 1, 2, 3, 'end' ])
+
+		expect(b_s2).deep.eq([ 3, 'end' ])
+		expect(b_b2).deep.eq([ 0, 1, 2, 3, 'end' ])
 	})
 
 	it('ends backforward', () =>
