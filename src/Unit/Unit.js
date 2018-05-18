7a6c79f8a3fbcb6d8445844b787d8f6e3031eed6
Strider <strange.transistor@gmail.com>
2018-05-19 01:03:48 +0300

    Unit: extend with options.

--- src/Unit/Unit.js
+++ src/Unit/Unit.js
@@ -8,17 +8,61 @@ import Pipe from './compose/Pipe'
 import Precursor from './compose/Precursor'
 import Fork from './compose/Fork'
 
+/* ::
+
+type $Options<$in, $prov: $Providers$Base, $out> =
+{
+	unit: $Unit$Fn<$in, $prov, $out>,
+	family?: string,
+}
+
+declare function Unit <$in, $prov: $Providers$Base, $out>
+(fn: $Unit$Fn<$in, $prov, $out>)
+: $Unit<$in, $prov, $out>
+
+declare function Unit <$in, $prov: $Providers$Base, $out>
+(options: $Options<$in, $prov, $out>)
+: $Unit<$in, $prov, $out>
+
+*/
+
 export default function Unit /* ::<$in, $prov: $Providers$Base, $out> */
 (
-	fn /* :$Unit$Fn<$in, $prov, $out> */
+	options /* :$Unit$Fn<$in, $prov, $out> | $Options<$in, $prov, $out> */
 )
 	/* :$Unit<$in, $prov, $out> */
 {
+	if (typeof options === 'function')
+	{
+		options =
+		{
+			unit: options
+		}
+	}
+
+	var fn = options.unit
+
+	/* main */
 	var unit = function (context)
 	{
 		return invoke(fn, context)
 	}
 
+	if (options.family)
+	{
+		unit.family = options.family
+	}
+	else
+	{
+		unit.family = Name()
+	}
+
+	unit[inspect.custom] = function inspect ()
+	{
+		return { family: unit.family }
+	}
+
+	/* Pipe, Pre, Fork as methods */
 	unit.pipe = function pipe /* ::<$out_next> */
 	(
 		next /* :$Unit<$out, $prov, $out_next> */
@@ -45,13 +89,6 @@ export default function Unit /* ::<$in, $prov: $Providers$Base, $out> */
 	{
 		return Fork(unit, side)
 	}
-
-	unit[inspect.custom] = function inspect ()
-	{
-		return { family: unit.family }
-	}
-
-	unit.family = Name()
 
 	return unit
 }
