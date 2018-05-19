86e4acc875bc8357d153fb0802e08560ce92eac5
Strider <strange.transistor@gmail.com>
2018-05-20 02:37:58 +0300

    Emptish: fix line-comment newline.

--- src/metal/Emptish.js
+++ src/metal/Emptish.js
@@ -5,14 +5,14 @@ import Content from '../Unit/Content'
 export default function Emptish ()
 {
 	var block = /\/\*\s*\*\//g
-	var line  = /\/\/\s*/g
+	var line  = /\/\/\s*\n/g
 
 	var multinw = /\n\n+/g
 
 	return Content(content =>
 	{
 		content = content.replace(block, '')
-		content = content.replace(line, '')
+		content = content.replace(line, '\n')
 		content = content.replace(multinw, '\n\n')
 
 		return content
