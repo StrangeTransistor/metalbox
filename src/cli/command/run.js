377f08e7cbb74c863320aad59c6e04472d0a2fe1
Strider <strange.transistor@gmail.com>
2018-05-30 00:43:03 +0300

    run: note todo.

--- src/cli/command/run.js
+++ src/cli/command/run.js
@@ -21,6 +21,7 @@ export default function (mini /* :minimistOutput */)
 {
 	console.log('mini', mini)
 
+	// TODO: attach runner here if required (to parse opts in corresponding section of cli)
 	var name = String(mini._[0] || '')
 
 	var resolved = basic_resolver(name)
