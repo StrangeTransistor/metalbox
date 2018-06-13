772aa82c1712697c67f52b315bcb2f57ff0f9e03
Strider <strange.transistor@gmail.com>
2018-06-13 21:10:56 +0300

    Less: test.

--- test/case/Unit/Less.test.js
+++ test/case/Unit/Less.test.js
@@ -1,32 +1,39 @@
 /* @flow */
 
-// import tmp from 'src/rootpath/tmp'
+import tmp from 'src/rootpath/tmp'
 import origin from 'src/rootpath/origin'
-// import collate from 'src/rootpath/collate'
+import collate from 'src/rootpath/collate'
+
+import compare from 'src/compare'
 
 import Entry from 'src/Entry'
 import Context from 'src/Context'
 
 import Load from 'src/Unit/Load'
+import Rebase from 'src/Unit/Rebase'
+import File from 'src/Unit/File'
 
 import Less from 'src/Unit/Less'
 
 describe('Less', () =>
 {
 	var less_org = origin('less')
-	// var es5_cl  = collate('es5')
+	var es5_cl  = collate('less')
 
 	it('Entry', async () =>
 	{
+		var tm = tmp()
+
 		var entry = Entry(less_org('index.less'))
 		var context = Context(entry)
 
-		var u = Load().pipe(Less())
+		var u = Load()
+		.pipe(Less())
+		.pipe(Rebase(less_org(), tm()))
+		.pipe(File.Entry())
 
-		var outcome = u(context)
+		await u(context).output
 
-		var output = await outcome.output
-
-		console.log(output.content.content)
+		compare(es5_cl(), tm())
 	})
 })
