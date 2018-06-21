0db3895e571348ed674b518bd88c8575e03c78ca
Strider <strange.transistor@gmail.com>
2018-06-21 11:55:57 +0300

    Debug: improve output of Entry(any) and Entry(File).

--- src/Unit/Debug.js
+++ src/Unit/Debug.js
@@ -1,6 +1,12 @@
 /* @flow */
 
+var NL = '\n'
+
 import { inspect } from 'util'
+
+import clc from 'cli-color'
+var { bold } = clc
+var file = clc.blue.italic
 
 import Unit from './Unit'
 
@@ -15,8 +21,51 @@ export default function Debug /* ::<$thru, $prov: $Providers$Base>*/ ()
 {
 	return Unit(it =>
 	{
-		process.stdout.write(inspect(it, defaults) + '\n')
+		if (it && it.filename && it.content)
+		{
+			/* @flow-off */
+			var entry = (it /* :$Entry<any> */)
+			debug_entry(entry)
+		}
+		else
+		{
+			write(inspect(it, defaults), '\n')
+		}
 
 		return it
 	})
+}
+
+
+function debug_entry (entry /* :$Entry<any> */)
+{
+	if (entry.content && entry.content.content)
+	{
+		write(bold('Entry<File>:'), ' ', entry.filename, NL)
+		write(file(entry.content.content), NL)
+		if (entry.content.sourcemap)
+		{
+			hr()
+			write(bold('sourcemap present:'), NL)
+			write(file(entry.content.sourcemap), NL)
+		}
+	}
+	else
+	{
+		write(bold('Entry<?>:'), ' ', entry.filename, NL)
+		write(inspect(entry.content, defaults), NL)
+	}
+	hr()
+}
+
+function hr ()
+{
+	write('--- --- ---', NL)
+}
+
+
+
+function write (...args /* :string[] */)
+{
+	process.stdout.write(args.join(''))
 }
