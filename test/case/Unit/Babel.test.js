4aa7042f61c1af4e5129a996f0b0af8072ef98b0
Strider <strange.transistor@gmail.com>
2018-06-23 01:22:29 +0300

    Babel: check minify preset.

--- test/case/Unit/Babel.test.js
+++ test/case/Unit/Babel.test.js
@@ -11,7 +11,7 @@ describe('Babel', () =>
 {
 	it('works', async () =>
 	{
-		var entry = Entry('babel.js', { content: 'var x = 1' })
+		var entry = Entry('babel.js', { content: 'var foo = 1' })
 		var context = Context(entry)
 
 		var b = Babel({ minified: true })
@@ -19,6 +19,20 @@ describe('Babel', () =>
 		var outcome = b(context)
 		var output = await outcome.output
 
-		expect(output.content.content).eq('var x=1;')
+		expect(output.content.content).eq('var foo=1;')
+	})
+
+	it('works', async () =>
+	{
+		var entry = Entry('babel.js', { content: 'var foo = 1\nvar bar = 2' })
+		var context = Context(entry)
+
+		// TODO: fix transient `babel-helper-evaluate-path`
+		var b = Babel({ presets: [ 'babel-preset-minify' ] })
+
+		var outcome = b(context)
+		var output = await outcome.output
+
+		expect(output.content.content).eq('var foo=1,bar=2;')
 	})
 })
