ca51122778e53216eb44534841898b3327357ea6
Strider <strange.transistor@gmail.com>
2018-06-22 13:40:56 +0300

    Less/test: use Load simpler.

--- test/case/Unit/Less.test.js
+++ test/case/Unit/Less.test.js
@@ -8,10 +8,10 @@ import collate from 'src/rootpath/collate'
 
 import compare from 'src/compare'
 
-import Entry from 'src/Entry'
 import Context from 'src/Context'
 
-import Load   from 'src/Unit/Entry/Load'
+import Load   from 'src/Unit/Load'
+
 import Rebase from 'src/Unit/Entry/Rebase'
 import Ext    from 'src/Unit/Entry/Ext'
 import File   from 'src/Unit/Entry/File'
@@ -22,22 +22,20 @@ import Postcss from 'src/Unit/Postcss'
 describe('Less', () =>
 {
 	var less_org = origin('less')
+	var context_null = Context(null)
 
 	it('Entry', async () =>
 	{
 		var tm = tmp()
 		var es5_cl  = collate('less/simple')
 
-		var entry = Entry(less_org('index.less'))
-		var context = Context(entry)
-
-		var u = Load()
+		var u = Load(less_org('index.less'))
 		.pipe(Less())
 		.pipe(Rebase(less_org(), tm()))
 		.pipe(Ext('less', 'css'))
 		.pipe(File())
 
-		await u(context).output
+		await u(context_null).output
 
 		compare(es5_cl(), tm())
 	})
@@ -47,17 +45,14 @@ describe('Less', () =>
 		var tm = tmp()
 		var es5_cl  = collate('less/prefix')
 
-		var entry = Entry(less_org('index.less'))
-		var context = Context(entry)
-
-		var u = Load()
+		var u = Load(less_org('index.less'))
 		.pipe(Less())
 		.pipe(Postcss([ autoprefixer([ 'last 4 version' ]) ]))
 		.pipe(Rebase(less_org(), tm()))
 		.pipe(Ext('less', 'css'))
 		.pipe(File())
 
-		await u(context).output
+		await u(context_null).output
 
 		compare(es5_cl(), tm())
 	})
