64d831bbee2116e9140363b61e6c93e439f86a85
Strider <strange.transistor@gmail.com>
2018-09-15 11:58:31 +0300

    Identity: family.

--- src/Unit/Identity.js
+++ src/Unit/Identity.js
@@ -4,5 +4,13 @@ import Unit from './Unit'
 
 export default function Identity /* ::<T> */ () /* :$Identity<T> */
 {
-	return Unit((it /* :T */) => it)
+	return Unit(
+	{
+		family: 'Identity',
+
+		unit (it /* :T */)
+		{
+			return it
+		},
+	})
 }
