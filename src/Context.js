251b92157ff54bf61ba817aa53bf4c34d92d3c79
Strider <strange.transistor@gmail.com>
2018-09-18 14:54:36 +0300

    tidy style.

--- src/Context.js
+++ src/Context.js
@@ -4,7 +4,9 @@ import Providers from './Providers'
 
 /* ::
 
-declare function Context<$in, $prov: $Providers$Base> ($in, $prov): $Context<$in, $prov>
+declare function Context<$in, $prov: $Providers$Base> ($in, $prov)
+: $Context<$in, $prov>
+
 declare function Context<$in> ($in): $Context<$in, {||}>
 
 */
