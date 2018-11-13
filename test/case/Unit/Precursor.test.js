2829f23c6c23fdb9a78014fa7b3b6f106f9ced6a
Strider <strange.transistor@gmail.com>
2018-11-14 00:26:22 +0300

    Precursor: more tests.

--- test/case/Unit/Precursor.test.js
+++ test/case/Unit/Precursor.test.js
@@ -29,18 +29,20 @@ describe.only('Precursor', () =>
 		})
 
 		var u = Precursor(u1, u2)
+		var r = u(Context({ x: 5 }))
 
-		var promise = await u(Context({ x: 5 })).promise
+		var buffer  = await concat(r.stream)
+		var promise = await r.promise
 
 		expect(promise).deep.eq({ y: 7 })
+		expect(buffer).deep.eq([ { y: 7 } ])
 	})
 
 	it('Precursor(u1, u2) throw', async () =>
 	{
-		var u1 = Unit(() =>
-		{
-			throw new Error('no_pass')
-		})
+		var e = new Error('no_pass')
+
+		var u1 = Unit(() => { throw e })
 
 		var u2 = Unit((_, context) =>
 		{
@@ -51,8 +53,12 @@ describe.only('Precursor', () =>
 
 		var u = Precursor(u1, u2)
 
-		await expect(u(Context({ x: 5 })).promise)
-		.rejectedWith('no_pass')
+		var r = u(Context({ x: 5 }))
+
+		var buffer = await concat(r.stream)
+
+		await expect(r.promise).rejectedWith('no_pass')
+		expect(buffer).deep.eq([ e ])
 	})
 
 	it('u1.pre(u2)', async () =>
@@ -73,9 +79,82 @@ describe.only('Precursor', () =>
 
 		var u = u1.pre(u2)
 
-		var promise = await u(Context({ x: '5' })).promise
+		var r = u(Context({ x: '5' }))
+
+		var buffer  = await concat(r.stream)
+		var promise = await r.promise
 
 		expect(promise).deep.eq({ y: '5a' })
+		expect(buffer).deep.eq([ { y: '5a' } ])
+	})
+
+	it('(u1 Error).pre(u2)', async () =>
+	{
+		var e = new Error('no_pass')
+
+		var u1 = Unit(() => { throw e })
+
+		var u2 = Unit(() => 1)
+
+		var u = u1.pre(u2)
+
+		var r = u(Context(null))
+
+		var buffer = await concat(r.stream)
+
+		await expect(r.promise).rejectedWith('no_pass')
+		expect(buffer).deep.eq([ e ])
+	})
+
+	it('async (u1 Error).pre(u2)', async () =>
+	{
+		var e = new Error('no_pass')
+
+		var u1 = Unit(async () => { throw e })
+		var u2 = Unit(async () => 1)
+
+		var u = u1.pre(u2)
+
+		var r = u(Context(null))
+
+		var buffer = await concat(r.stream)
+
+		await expect(r.promise).rejectedWith('no_pass')
+		expect(buffer).deep.eq([ e ])
+	})
+
+	it('(u1).pre(u2 Error)', async () =>
+	{
+		var e = new Error('no_pass')
+
+		var u1 = Unit(() => 1)
+		var u2 = Unit(() => { throw e })
+
+		var u = u1.pre(u2)
+
+		var r = u(Context(null))
+
+		var buffer = await concat(r.stream)
+
+		await expect(r.promise).rejectedWith('no_pass')
+		expect(buffer).deep.eq([ e ])
+	})
+
+	it('async (u1).pre(u2 Error)', async () =>
+	{
+		var e = new Error('no_pass')
+
+		var u1 = Unit(async () => 1)
+		var u2 = Unit(async () => { throw e })
+
+		var u = u1.pre(u2)
+
+		var r = u(Context(null))
+
+		var buffer = await concat(r.stream)
+
+		await expect(r.promise).rejectedWith('no_pass')
+		expect(buffer).deep.eq([ e ])
 	})
 
 	it('(stream u1).pre(u2)', async () =>
@@ -102,11 +181,42 @@ describe.only('Precursor', () =>
 		var u = u1.pre(u2)
 		var result = u(Context({ x: '5' }))
 
-		var promise = result.promise
-		var buffer = concat(result.stream)
+		var buffer  = await concat(result.stream)
+		var promise = await result.promise
 
-		expect(await promise).eq('5a')
-		expect(await buffer).deep.eq([ '5a', '5a', '5a' ])
+		expect(promise).eq('5a')
+		expect(buffer).deep.eq([ '5a', '5a', '5a' ])
+	})
+
+	it('async (stream u1).pre(u2)', async () =>
+	{
+		var u1 = Unit(async (input) =>
+		{
+			expect(input).deep.eq({ x: '5' })
+
+			return replay(
+			[
+				{ x: 1 },
+				{ x: 2 },
+				{ x: 3 },
+			])
+		})
+
+		var u2 = Unit(async (input) =>
+		{
+			var x /* :string */ = input.x
+
+			return x + 'a'
+		})
+
+		var u = u1.pre(u2)
+		var result = u(Context({ x: '5' }))
+
+		var buffer  = await concat(result.stream)
+		var promise = await result.promise
+
+		expect(promise).eq('5a')
+		expect(buffer).deep.eq([ '5a', '5a', '5a' ])
 	})
 
 	it('(stream u1 Error).pre(u2)', async () =>
@@ -137,8 +247,46 @@ describe.only('Precursor', () =>
 		var result = u(Context({ x: '5' }))
 
 		var r = result.promise.then(
-		()  => expect(false).true,
-		(e) => e)
+			()  => expect(false).true,
+			(e) => e
+		)
+		var buffer = concat(result.stream)
+
+		expect(await r).eq(error)
+		expect(await buffer).deep.eq([ '5a', '5a', error ])
+	})
+
+	it('async (stream u1 Error).pre(u2)', async () =>
+	{
+		var error = new Error('e')
+
+		var u1 = Unit(async (input) =>
+		{
+			expect(input).deep.eq({ x: '5' })
+
+			return replay(
+			[
+				{ x: 1 },
+				{ x: 2 },
+				error,
+				error,
+			])
+		})
+
+		var u2 = Unit(async (input) =>
+		{
+			var x /* :string */ = input.x
+
+			return x + 'a'
+		})
+
+		var u = u1.pre(u2)
+		var result = u(Context({ x: '5' }))
+
+		var r = result.promise.then(
+			()  => expect(false).true,
+			(e) => e
+		)
 		var buffer = concat(result.stream)
 
 		expect(await r).eq(error)
